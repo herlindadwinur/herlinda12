b76d9e797c74a4f5fab6e7331098f809c55cb365
herlindadwinur <130634278+herlindadwinur@users.noreply.github.com>
2023-07-06 12:00:36 +0700

    Add files via upload

---
+++ pong.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:gamepongherlinda/pemukul.dart';
+
+import 'bola.dart';
+
+class Pong extends StatefulWidget {
+  const Pong({Key? key}) : super(key: key);
+
+  @override
+  State<Pong> createState() => _PongState();
+}
+
+class _PongState extends State<Pong> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 200,
+              child: Bola(diameter: 35),
+            ),
+            Positioned(bottom: 0, child: Pemukul(height: 20, width: 70)),
+          ],
+        );
+      },
+    );
+  }
+}
